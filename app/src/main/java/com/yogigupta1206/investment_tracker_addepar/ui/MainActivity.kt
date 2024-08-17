81bdbe03caab1a7af27fae9f57d12d15b75e79d5
Yogesh Gupta <yogesh1.gupta@paytm.com>
2024-08-17 16:24:55 +0530

    MainActivity setup

--- app/src/main/java/com/yogigupta1206/investment_tracker_addepar/ui/MainActivity.kt
+++ app/src/main/java/com/yogigupta1206/investment_tracker_addepar/ui/MainActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.yogigupta1206.investment_tracker_addepar.ui.components.AppNavigationGraph
 import com.yogigupta1206.investment_tracker_addepar.ui.theme.InvestmentTrackerAddeparTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,11 +22,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             InvestmentTrackerAddeparTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.White)
+                ) {
+                    AppEntryPoint()
                 }
             }
         }
@@ -33,17 +35,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AppEntryPoint() {
+    AppNavigationGraph()
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     InvestmentTrackerAddeparTheme {
-        Greeting("Android")
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+        ) {
+            AppEntryPoint()
+        }
     }
 }
